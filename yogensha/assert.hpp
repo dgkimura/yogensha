d59a650a79e8cc6e8c865550cd8e56fdd717513f
David Kimura <dkimura@pivotal.io>
2017-02-14 17:03:54 -0800

    Add travis-ci config

--- yogensha/assert.hpp
+++ yogensha/assert.hpp
@@ -1,9 +1,6 @@
-#ifndef YOGENSHA_ASSERT_HPP
-#define YOGENSHA_ASSERT_HPP
+#pragma once
 
 // A very simple solution to compile-time assertions. If expr evaluates to
-// non-zero then the compiler will signal a compilet-time error because it
+// non-zero then the compiler will signal a compile-time error because it
 // is illegal to construct a negative length array..
 #define static_assert(expr) { char unnamed[(expr) ? 1 : -1]; }
-
-#endif
