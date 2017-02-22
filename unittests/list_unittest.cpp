985b0be8a1167eaf21892cfcc890eec3dada823f
David Kimura <dkimura@pivotal.io>
2017-02-22 13:39:18 -0800

    Add list operations prepend, append, and front

--- unittests/list_unittest.cpp
+++ unittests/list_unittest.cpp
@@ -6,3 +6,57 @@ void test_size_of_list()
     constexpr int i = yogensha::size<yogensha::list<int, float, void>>::value;
     static_assert(i == 3);
 }
+
+void test_list_size()
+{
+    constexpr int i = yogensha::size<
+        yogensha::list<
+            yogensha::integral_constant<int, 4>,
+            yogensha::integral_constant<int, 2>
+        >
+    >::value;
+
+    static_assert(i == 2);
+}
+
+void test_list_append()
+{
+    constexpr int i = yogensha::size<
+        yogensha::append<
+            yogensha::list<
+                yogensha::integral_constant<int, 4>,
+                yogensha::integral_constant<int, 2>
+            >,
+            yogensha::integral_constant<int, 1>
+        >::type
+    >::value;
+
+    static_assert(i == 3);
+}
+
+void test_list_prepend()
+{
+    constexpr auto i = yogensha::front<
+        yogensha::prepend<
+            yogensha::integral_constant<int, 6>,
+            yogensha::list<
+                yogensha::integral_constant<int, 4>,
+                yogensha::integral_constant<int, 2>
+            >
+        >
+    >::type::value;
+
+    static_assert(i == 6);
+}
+
+void test_list_front()
+{
+    constexpr auto i = yogensha::front<
+        yogensha::list<
+            yogensha::integral_constant<int, 4>,
+            yogensha::integral_constant<int, 2>
+        >
+    >::type::value;
+
+    static_assert(i == 4);
+}
--- yogensha/list.hpp
+++ yogensha/list.hpp
@@ -14,7 +14,10 @@ struct list
 
 // At this point the nature of List parameter is unknown, and so we cannot give
 // size a proper definition.
-template <typename List> struct size;
+template <typename List>
+struct size
+{
+};
 
 // Here we partially specialize size.
 //   - List:
@@ -24,10 +27,49 @@ template <typename List> struct size;
 //         A template parameter pack.
 //   - Elements...
 //         Triggers an expansion of every type in the pack.
-template <template<class ...> class List, class... Elements>
+template <template<typename ...> class List, typename... Elements>
 struct size<List<Elements...>>
     : yogensha::integral_constant<std::size_t, sizeof...(Elements)>
 {
+};
+
+// At this point the nature of List parameter is unknown, and so we cannot give
+// size a proper definition.
+template <typename List, typename Element>
+struct append
+{
+};
+
+template <template <typename ...> class List, typename... Elements, typename E>
+struct append<List<Elements...>, E>
+{
+    using type = List<Elements..., E>;
+};
+
+// At this point the nature of List parameter is unknown, and so we cannot give
+// size a proper definition.
+template <typename List, typename Element>
+struct prepend
+{
+};
+
+template <template <typename ...> class List, typename E, typename... Elements>
+struct prepend<E, List<Elements...>>
+{
+    using type = List<E, Elements...>;
+};
+
+// At this point the nature of List parameter is unknown, and so we cannot give
+// size a proper definition.
+template <typename List>
+struct front
+{
+};
+
+template <template <typename ...> class List, typename E, typename... Elements>
+struct front<List<E, Elements...>>
+{
+    using type = E;
 };
 
 }
